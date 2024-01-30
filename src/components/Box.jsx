5881da6d28558b4ea463ba467beca4fb7163ce39
Kiana Masqati <masqati.kiana@gmail.com>
2024-01-30 18:09:00 +0000

    Add box animation

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -4,10 +4,11 @@ const Box = () => {
   return (
     <>
       <div className="box">
-        <div className="inhale"></div>
+        {/* <div className="inhale"></div>
         <div className="hold-inhale"></div>
         <div className="exhale"></div>
-        <div className="hold-exhale"></div>
+        <div className="hold-exhale"></div> */}
+        <div className="circle"></div>
       </div>
     </>
   );
