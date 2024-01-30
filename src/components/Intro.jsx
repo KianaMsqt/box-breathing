0c92b41a2cde41015856a1e3bf3e9b08867702cb
Kiana Masqati <masqati.kiana@gmail.com>
2024-01-30 15:54:37 +0000

    Add intro

---
+++ src/components/Intro.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div className="intro">
+      <h2>
+        is a breathing technique. It can heighten performance and concentration
+        while also being a powerful stress reliever.
+      </h2>
+      <p>
+        Before you get started, make sure that you’re seated upright in a
+        comfortable chair with your feet flat on the floor. Try to be in a
+        stress-free, quiet environment where you can focus on your breathing.
+      </p>
+      <p>
+        Keeping your hands relaxed in your lap with your palms facing up, focus
+        on your posture. You should be sitting up straight. This will help you
+        take deep breaths.
+      </p>
+      <p>When you’re ready, start.</p>
+      <button>Start</button>
+    </div>
+  );
+}
+
+export default Intro;
